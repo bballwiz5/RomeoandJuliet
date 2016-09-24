3d9bb54e3e3c67ba956da3a5d1a513bd2dc1e271
Tracy Jacobson <tracyjacobson@Tracys-MacBook-Air.local>
2016-09-24 16:56:33 -0400

    buttons

--- app/src/main/java/com/philipandmegan/romeoandjuliet/Profile.java
+++ app/src/main/java/com/philipandmegan/romeoandjuliet/Profile.java
@@ -2,6 +2,7 @@ package com.philipandmegan.romeoandjuliet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Profile extends AppCompatActivity {
 
@@ -10,4 +11,14 @@ public class Profile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
     }
+
+    /** Called when the user touches the button */
+    public void signUp(View view) {
+        setContentView(R.layout.sign_up_screen);
+    }
+
+    public void signIn(View view) {
+        setContentView(R.layout.sign_in_screen);
+    }
+
 }
